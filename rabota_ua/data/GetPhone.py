5a4df9cadaee245f8ffb14d4a42fa1e6745d828c
raingang <raingang4@gmail.com>
2017-07-20 21:29:09 +0300

    Added сollection phones and links:squirrel:

---
+++ rabota_ua/data/GetPhone.py
@@ -0,0 +1,13 @@
+import requests, csv
+from bs4 import BeautifulSoup
+def get_html(url):
+	r = requests.get(url)
+	return r.text
+def get_phone_number(url):	
+	html = get_html(url)
+	soup = BeautifulSoup(html, 'lxml')
+	try:
+		data = soup.find('span', class_ = 'opencontact').text.strip()
+	except:
+		data = ''
+	return data
